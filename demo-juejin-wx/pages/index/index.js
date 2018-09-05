42ab398c14f6ac0fbf77a8481b31a90092642d23
xjrj <xjrj@xjrj.local>
2018-09-05 18:14:37 +0800

    小册数据接口

--- demo-juejin-wx/pages/index/index.js
+++ demo-juejin-wx/pages/index/index.js
@@ -1,54 +1,90 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const config = getApp().globaData.config
+let globalData = getApp().globalData
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  data:{
+    COUNT:20,
+    timeline:[],
+    hotRecomment:[],
+    hotRrecommendShow:true,
+    auth:{},
+    logined:true,
+    rotate:'',
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+  onShow(){
+    if(utils.pageReload(this.data.auth,[this.data.timeline])){
+      wx.startPullDownRefresh({})
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+
+  onPullDownRefresh(){
+    this.init()
+  },
+
+  init(){
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hotRrecommendShow:this.data.hotRrecommendShow,
+      auth:{},
+      rotate:'', 
     })
-  }
+
+    let auth = utils.ifLogined()
+    this.setData({
+      auth,
+      logined:auth,
+    })
+    this.getBannerImgList()
+    this.getEntryByTimeline(true)
+    if(auth){
+      this.getEntryByHotRecomment()
+    }
+  },
+
+  getBannerImgList(){
+    const auth = this.data.auth
+    wx.request({
+      url: `${config.bannerRequse}/get_banner`,
+      data:{
+        position:'explore',
+        page:0,
+        pageSize:20,
+        platform:'android',
+        device_id:auth.clientId,
+        client_id:auth.clientId,
+        token:auth.token,
+        src:'android',
+      },
+
+      success:(res) =>{
+        let data = res.data
+        if(data.s === 1){
+          let bannerImgList = (data.d && data.d.banner) || []
+          wx.setStorage({
+            key: 'bannerImgList',
+            data: bannerImgList,
+          })
+        }else{
+          wx.showToast({
+            title: data.m.toString(),
+            icon:'none',
+          })
+        }
+      },
+      fail:() =>{
+        wx.showToast({
+          title: 'error',
+          icon:'none',
+        })
+      },
+    })
+  },
+  
+  
+
+  onLoad: function () {
+    
+  },
 })
--- demo-juejin-wx/pages/xiaoce/xiaoce.js
+++ demo-juejin-wx/pages/xiaoce/xiaoce.js
@@ -1,18 +1,107 @@
 // pages/xiaoce/xiaoce.js
+const config = getApp().globalData.config
+const utils = require('../../utils/util.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    xiaoceList:["1","2"]
+
+    xiaoceList:[],
+    noResult:false,
+    auth:{},
+    pageNum:1,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.init()
+  },
+
+  onPullDownRefresh(){
+    this.reload()
+  },
+  reload(reload){
+    this.setData({
+      noResult:false,
+      auth:{},
+      pageNum:1,
+    })
+    this.init(reload)
+  },
+  init(reload){
+    let auth = utils.ifLogined()
+    this.setData({
+      auth,
+    })
+    this.getXiaoce(reload)
+  },
+
+  getXiaoce(reload){
+    let auth = this.data.auth
+    if(reload){
+      this.setData({
+        pageNum:1,
+      })
+    }
+    wx.request({
+      url: `${config.xiaoceRequestUrl}/getListByLastTime`,
+      data:{
+        src:'web',
+        uid:auth.uid || '',
+        device_id:auth.clientId,
+        token:auth.token,
+        pageNum:this.data.pageNum,
+      },
+
+      success: (res) => {
+        let data = res.data
+        if (data.s === 1) {
+          let list = data.d
+          if (!utils.isEmptyObject(list)) {
+            let pageNum = this.data.pageNum + 1
+            this.setData({
+              pageNum,
+              xiaoceList: reload ? list : this.data.xiaoceList.concat(list),
+            })
+          }
+        }else{
+          if(data.s == 2){
+            this.setData({
+              noResult:true,
+            })
+          }else{
+            wx.showToast({
+              title: data.m.toString(),
+              icon:'none',
+            })
+          }
+        }
+      },
+
+      fail:() => {
+        wx.showToast({
+          title:'errer',
+          icon:'none',
+        })
+      },
+      complete:() =>{
+        wx.stopPullDownRefresh()
+      },
+    })
+  },
+
+  onReachBottom(){
+    if(!this.data.xiaoceList.length || !this.data.noResult){
+      this.getXiaoce()
+    }
+  },
+  onShareAppMessage(res){
+    return{}
   },
 
   /**
