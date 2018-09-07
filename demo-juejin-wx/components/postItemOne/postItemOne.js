d4e309ea885da8b4a64eb30f56f93a7d2dfc61e9
xjrj <xjrj@xjrj.local>
2018-09-07 15:51:28 +0800

    home and launch update

--- demo-juejin-wx/components/postItemOne/postItemOne.js
+++ demo-juejin-wx/components/postItemOne/postItemOne.js
@@ -1,4 +1,22 @@
+let utils = require('../../utils/util.js')
+
 Component({
+  properties:{
+    list:{
+      type:Array,
+      value:[]
+    },
+    graphics:{
+      type:Boolean,
+      value:false,
+    },
+  },
+
+  methods:{
+    toPostDeail(e){
+      utils.toPostDeail(e)
+    },
+  },
 
   
 })
--- demo-juejin-wx/components/postItemTwo/postItemTwo.js
+++ demo-juejin-wx/components/postItemTwo/postItemTwo.js
@@ -1,4 +1,24 @@
-Component({
+let utils = require('../../utils/util.js')
 
-  
+Component({
+  properties:{
+    list:{
+      type:Array,
+      value:[]
+    },
+  },
+
+  metheds:{
+    toPostDetail(e){
+      utils.toPostDetail(e)
+    },
+
+    toPerdonal(e){
+      let item = e.currentTarget.dataset.item
+      wx.navigateTo({
+        url: '',
+      })
+    }
+  }
+
 })
